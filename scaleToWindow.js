ddd22ede8f7129e01f5cbfa718fb40a17e67599c
Rex <dandylion13@gmail.com>
2015-06-10 13:05:24 -0400

    First commit

---
+++ scaleToWindow.js
@@ -0,0 +1,75 @@
+function scaleToWindow(canvas, backgroundColor) {
+
+  backgroundColor = backgroundColor || "#2C3539";
+  var scaleX, scaleY, scale, center;
+  
+  //1. Scale the canvas to the correct size
+  //Figure out the scale amount on each axis
+  scaleX = window.innerWidth / canvas.width;
+  scaleY = window.innerHeight / canvas.height;
+
+  //Scale the canvas based on whichever value is less: `scaleX` or `scaleY`
+  scale = Math.min(scaleX, scaleY);
+  canvas.style.transformOrigin = "0 0";
+  canvas.style.transform = "scale(" + scale + ")";
+
+  //2. Center the canvas.
+  //Decide whether to center the canvas vertically or horizontally.
+  //Wide canvases should be centered vertically, and 
+  //square or tall canvases should be centered horizontally
+  if (canvas.width > canvas.height) {
+    if (canvas.width * scale < window.innerWidth) {
+      center = "horizontally";
+    } else { 
+      center = "vertically";
+    }
+  } else {
+    if (canvas.height * scale < window.innerHeight) {
+      center = "vertically";
+    } else { 
+      center = "horizontally";
+    }
+  }
+  
+  //Center horizontally (for square or tall canvases)
+  var margin;
+  if (center === "horizontally") {
+    margin = (window.innerWidth - canvas.width * scale) / 2;
+    canvas.style.marginLeft = margin + "px";
+    canvas.style.marginRight = margin + "px";
+  }
+
+  //Center vertically (for wide canvases) 
+  if (center === "vertically") {
+    margin = (window.innerHeight - canvas.height * scale) / 2;
+    canvas.style.marginTop = margin + "px";
+    canvas.style.marginBottom = margin + "px";
+  }
+
+  //3. Remove any padding from the canvas  and body and set the canvas
+  //display style to "block"
+  canvas.style.paddingLeft = 0;
+  canvas.style.paddingRight = 0;
+  canvas.style.paddingTop = 0;
+  canvas.style.paddingBottom = 0;
+  canvas.style.display = "block";
+  
+  //4. Set the color of the HTML body background
+  document.body.style.backgroundColor = backgroundColor;
+  
+  //Fix some quirkiness in scaling for Safari
+  var ua = navigator.userAgent.toLowerCase(); 
+  if (ua.indexOf("safari") != -1) { 
+    if (ua.indexOf("chrome") > -1) {
+      // Chrome
+    } else {
+      // Safari
+      canvas.style.maxHeight = "100%";
+      canvas.style.minHeight = "100%";
+    }
+  }
+
+  //5. Return the `scale` value. This is important, because you'll nee this value 
+  //for correct hit testing between the pointer and sprites
+  return scale;
+};
